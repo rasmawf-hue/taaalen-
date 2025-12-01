6a19a40e6b160f3456612ad6db260eabf45e611f
rasmawf-hue <rasmawf@gmail.com>
2025-12-02 00:17:43 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // هذا مهم جداً لتعمل الروابط النسبية
   build: {
+    target: 'es2015', // يضمن كود متوافق ونظيف
+    sourcemap: false, // يمنع توليد خرائط المصدر اللي أحياناً تستخدم eval
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
   }
 });
