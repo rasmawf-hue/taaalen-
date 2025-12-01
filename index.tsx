59b7eff6093fafda09c45fb2b496d764381f0baf
rasmawf-hue <rasmawf@gmail.com>
2025-12-01 18:28:49 +0200

    Updated index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <--- هذا هو السطر المفقود!
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ... باقي الكود كما هو
